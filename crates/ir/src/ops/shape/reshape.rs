9c62b312e7c27977c400ecf2fc6423554f89d30a
FL33TW00D <fleetwoodpersonal@gmail.com>
2022-10-09 22:02:44 +0100

    chore: we running ResNet baby

--- crates/ir/src/ops/shape/reshape.rs
+++ crates/ir/src/ops/shape/reshape.rs
@@ -1,9 +1,9 @@
 use onnx::onnx_pb;
-use smallvec::{smallvec, SmallVec};
+use smallvec::smallvec;
 use std::borrow::Cow;
 
 use crate::{
-    as_std, validate_providers, BoxOp, DType, DataType, IntoArcTensor, Op, OpGroup, PVec,
+    as_std, shape, validate_providers, BoxOp, DType, DataType, IntoArcTensor, Op, OpGroup, PVec,
     RealizedOp, Shape, Tensor,
 };
 #[derive(Debug, Clone)]
@@ -13,11 +13,34 @@ pub struct Reshape {
 
 impl Reshape {
     pub fn reshape<D: DataType + ndarray::LinalgScalar + num::NumCast>(
+        original_shape: Shape,
         shape_tensor: &Tensor,
     ) -> Shape {
-        let data: Vec<D> = shape_tensor.as_slice().unwrap().into();
-        let mut new_shape = SmallVec::new();
-        for elem in data {
+        let mut shape_data: Vec<D> = shape_tensor.as_slice().unwrap().into();
+
+        let mut product = D::one();
+        let mut unknown_dim = None;
+        for (i, dim) in shape_data.iter().enumerate() {
+            if *dim == D::from(-1).unwrap() {
+                if unknown_dim.is_some() {
+                    panic!("Reshape: only one unknown dimension is allowed");
+                }
+                unknown_dim = Some(i);
+            } else if *dim != D::zero() {
+                product = product * *dim;
+            }
+        }
+
+        println!("PRODUCT: {:?}", product);
+
+        if let Some(unknown_dim) = unknown_dim {
+            shape_data[unknown_dim] =
+                D::from(original_shape.iter().product::<usize>() / product.to_usize().unwrap())
+                    .unwrap();
+        }
+
+        let mut new_shape = shape!();
+        for elem in shape_data {
             new_shape.push(num::cast(elem).unwrap_or_else(|| panic!("Failed to cast: {:?}", elem)));
         }
 
@@ -35,9 +58,13 @@ impl Op for Reshape {
     }
 
     fn realize(&self, providers: PVec) -> anyhow::Result<RealizedOp> {
+        println!("Allow zero: {:?}", self.allow_zero);
         println!("Reshape providers: {:?}", providers);
         validate_providers(&providers, 2, 2, &self.name())?;
-        let new_shape = as_std!(Reshape::reshape(providers[1].dt)(&providers[1]));
+        let new_shape = as_std!(Reshape::reshape(providers[1].dt)(
+            providers[0].shape.clone(),
+            &providers[1]
+        ));
 
         let reshaped = Tensor::new(providers[0].dt, new_shape).into_arc_tensor();
 
@@ -47,5 +74,6 @@ impl Op for Reshape {
 
 pub fn build_reshape(proto: &onnx_pb::NodeProto) -> Result<BoxOp, anyhow::Error> {
     let allow_zero = proto.get_attribute("allowzero", Some(0))?;
+    println!("ALLOW ZERO: {:?}", allow_zero);
     Ok(Box::new(Reshape { allow_zero }) as BoxOp)
 }
