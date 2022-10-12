9f111e307036977766988995f0acb98b7c9eab31
FL33TW00D <fleetwoodpersonal@gmail.com>
2022-10-12 09:11:37 +0100

    chore: colourful

--- crates/ir/src/op_group.rs
+++ crates/ir/src/op_group.rs
@@ -21,10 +21,24 @@ pub enum OpGroup {
 lazy_static! {
     pub static ref COLOUR_MAP: HashMap<OpGroup, &'static str> = {
         let mut m = HashMap::new();
-        m.insert(OpGroup::Activation, "salmon");
+        m.insert(OpGroup::Activation, "indianred");
         m.insert(OpGroup::Constant, "lightgray");
         m.insert(OpGroup::Data, "lightgray");
-        m.insert(OpGroup::Layer, "springgreen");
+        m.insert(OpGroup::Layer, "mediumseagreen");
+        m.insert(OpGroup::Normalization, "dodgerblue");
+        m.insert(OpGroup::Transform, "sandybrown");
+        m.insert(OpGroup::Tensor, "skyblue");
+        m
+    };
+    pub static ref SHAPE_MAP: HashMap<OpGroup, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(OpGroup::Activation, "box");
+        m.insert(OpGroup::Constant, "diamond");
+        m.insert(OpGroup::Data, "ellipse");
+        m.insert(OpGroup::Layer, "diamond");
+        m.insert(OpGroup::Normalization, "ellipse");
+        m.insert(OpGroup::Transform, "box");
+        m.insert(OpGroup::Tensor, "ellipse");
         m
     };
 }
